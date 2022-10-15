6a13e5c305350729d2cb039128aa9508112bac5f
nejdetkadir <nejdetkadir.550@gmail.com>
2022-10-16 00:31:47 +0300

    Initial commit

---
+++ lib/phlex/heroicons.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'heroicons/version'
+
+module Phlex
+  module Heroicons
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/phlex/heroicons/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Phlex
+  module Heroicons
+    VERSION = '0.1.0'
+  end
+end
